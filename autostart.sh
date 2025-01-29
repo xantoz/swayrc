b5cb19b3991ed973e97ebec0261ec165909f8fb5
xantoz <bountyjedi@gmail.com>
2025-01-29 11:14:09 +0100

    autostart.sh: Import some various variables into the systemd user environment
    This is typically needed for portals and the likes

--- autostart.sh
+++ autostart.sh
@@ -2,3 +2,9 @@
 
 #pgrep xss-lock > /dev/null || xss-lock -- physlock -s &
 [ -f ~/.Xresources ] && xrdb -merge ~/.Xresources &
+
+# Import some various variables into the systemd user environment
+if which systemctl 2> /dev/null > /dev/null; then
+    systemctl --user import-environment WAYLAND_DISPLAY
+    systemctl --user import-environment DISPLAY
+fi
