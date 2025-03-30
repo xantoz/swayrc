0a48fc7760b51d0efc41c9abe252fe2f168b879d
xantoz <bountyjedi@gmail.com>
2025-03-30 21:02:54 +0200

    Some handling of automagic variable sandshit needed on systemd systems
    The second part is commented out as it seems unneccessary since
    I found the --systemd param to dbus-update-activation-environment.
    However I don't completely trust that yet so keep the systemcl --user
    import-environment things in a comment for now

--- autostart.sh
+++ autostart.sh
@@ -3,8 +3,20 @@
 #pgrep xss-lock > /dev/null || xss-lock -- physlock -s &
 [ -f ~/.Xresources ] && xrdb -merge ~/.Xresources &
 
-# Import some various variables into the systemd user environment
-if which systemctl 2> /dev/null > /dev/null; then
-    systemctl --user import-environment WAYLAND_DISPLAY
-    systemctl --user import-environment DISPLAY
+
+export XDG_CURRENT_DESKTOP=sway
+
+# TODO: Can all this stuff be handled by a sway systemd service?
+if which dbus-update-activation-environment 2> /dev/null > /dev/null; then
+    dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP
+    dbus-update-activation-environment --systemd XDG_SEAT XDG_SESSION_ID XDG_VTNR
 fi
+
+# # Import some various variables into the systemd user environment
+# if which systemctl 2> /dev/null > /dev/null; then
+#     systemctl --user import-environment WAYLAND_DISPLAY
+#     systemctl --user import-environment DISPLAY
+#     systemctl --user import-environment XDG_CURRENT_DESKTOP
+#     systemctl --user import-environment XDG_SESSION_DESKTOP
+#     systemctl --user import-environment SWAYSOCK
+# fi
